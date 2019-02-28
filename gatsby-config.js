d3bed33ba1b59e5aa6348d3ed0fc93412fd1319c
Tristan Ashley <trisashley@gmail.com>
2019-02-28 14:40:22 +0000

    feat: Update site name, add site icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `tristanashley.com`,
+        short_name: `tristanashley.com`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#074068`,
+        theme_color: `#074068`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/site-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
