0d20a7b77598a5f35340be7237f9f6d20fcd33ed
Tristan Ashley <trisashley@gmail.com>
2019-03-08 15:35:06 +0000

    feat: remove base included footer, content. Update site meta data

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,6 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
