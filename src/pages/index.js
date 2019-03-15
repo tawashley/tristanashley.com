d45a04ba07fb169cea83306c27ed4eb0b5caee03
Tristan Ashley <trisashley@gmail.com>
2019-03-15 11:30:36 +0000

    feat: Add short paragraph, links to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,6 +9,26 @@ const IndexPage = () => (
     <h2>
         Hi there, I'm a front end developer that makes 'internet things'.
     </h2>
+    <p>
+        There isn't much to see here really, but I do have a few links that go to here, there and everywhere
+    </p>
+    <ul>
+        <li>
+            <a href="https://github.com/tawashley">
+                GitHub - things I've made
+            </a>
+        </li>
+        <li>
+            <a href="https://www.linkedin.com/in/tristanashley">
+                LinkedIn - things I've done
+            </a>
+        </li>
+        <li>
+            <a href="https://twitter.com/tawashley">
+                Twitter - things I've said
+            </a>
+        </li>
+    </ul>
 
   </Layout>
 )
