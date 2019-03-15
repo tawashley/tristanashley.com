ab65cd918e93ca8c0562263f4016e2eb26269aa0
Tristan Ashley <trisashley@gmail.com>
2019-03-15 12:11:49 +0000

    refactor: Change IndexPage to a class components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,34 +3,34 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h2 style={{ marginTop: 0 }}>
-        Hi there, I'm a front end developer that makes 'internet things'.
-    </h2>
-    <p>
-        There isn't much to see here really, but I do have a few links that go to 'here, there and everywhere'.
-    </p>
-    <ul>
-        <li>
-            <a href="https://github.com/tawashley">
-                GitHub - things I've made
-            </a>
-        </li>
-        <li>
+export default class IndexPage extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" />
+        <h2 style={{ marginTop: 0 }}>
+          Hi there, I'm a front end developer that makes 'internet things'.
+        </h2>
+        <p>
+          There isn't much to see here really, but I do have a few links that go
+          to 'here, there and everywhere'.
+        </p>
+        <ul>
+          <li>
+            <a href="https://github.com/tawashley">GitHub - things I've made</a>
+          </li>
+          <li>
             <a href="https://www.linkedin.com/in/tristanashley">
-                LinkedIn - things I've done
+              LinkedIn - things I've done
             </a>
-        </li>
-        <li>
+          </li>
+          <li>
             <a href="https://twitter.com/tawashley">
-                Twitter - things I've said
+              Twitter - things I've said
             </a>
-        </li>
-    </ul>
-
-  </Layout>
-)
-
-export default IndexPage
+          </li>
+        </ul>
+      </Layout>
+    )
+  }
+}
